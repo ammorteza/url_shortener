c292064fc32309c168dd3ae34eaa8a7b77fe1cc0
morteza amzajerdi <ammorteza@yahoo.com>
2019-09-24 09:38:24 +0330

    fixed a bug in make url API 2019/09/24

--- controllers/url_controller.go
+++ controllers/url_controller.go
@@ -6,7 +6,6 @@ import (
 	"github.com/ammorteza/url_shortener/models"
 	"github.com/gorilla/mux"
 	"github.com/nu7hatch/gouuid"
-	"log"
 	"net/http"
 )
 
@@ -28,40 +27,29 @@ func NewUrlController(connection db.DbConnection) *UrlController{
 	return urlController
 }
 
-func (c *UrlController) MakeUrl(w http.ResponseWriter, r *http.Request) {
+func (c *UrlController) MakeUrl(w http.ResponseWriter, r *http.Request){
 	urlModel, err := models.NewUrlModel(c.dbConnection)
 	if err != nil{
-		log.Fatal(err)
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write(nil)
+		http.Error(w, "error in make url model: " + err.Error(), http.StatusInternalServerError)
 	}else{
 		requestBody := requestBody{}
-		err = json.NewDecoder(r.Body).Decode(&requestBody)
-		if err = json.NewDecoder(r.Body).Decode(&requestBody); err != nil {
-			log.Fatal(err)
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write(nil)
+		if err := json.NewDecoder(r.Body).Decode(&requestBody); err != nil {
+			http.Error(w, "error in get request params: " + err.Error(), http.StatusInternalServerError)
 		}else{
 			responseBody := ResponseBody{}
 			hashedUrl, err := uuid.NewV4()
 
 			if err != nil {
-				log.Fatal(err)
-				w.WriteHeader(http.StatusInternalServerError)
-				w.Write(nil)
+				http.Error(w, "error in make unique url (uuid):" + err.Error(), http.StatusInternalServerError)
 			} else {
 				responseBody.Short_url = "/" + hashedUrl.String()
 
 				if err := urlModel.Insert(requestBody.Base_url, hashedUrl.String()); err != nil {
-					log.Fatal(err)
-					w.WriteHeader(http.StatusInternalServerError)
-					w.Write(nil)
+					http.Error(w, "error in insert new record in db: " + err.Error(), http.StatusInternalServerError)
 				} else {
 					response, err := json.Marshal(responseBody)
 					if err != nil {
-						log.Fatal(err)
-						w.WriteHeader(http.StatusInternalServerError)
-						w.Write(nil)
+						http.Error(w, "error in make response: " + err.Error(), http.StatusInternalServerError)
 					} else {
 						w.Header().Set("Content-Type", "application/json")
 						w.WriteHeader(http.StatusOK)
@@ -76,12 +64,12 @@ func (c *UrlController) MakeUrl(w http.ResponseWriter, r *http.Request) {
 func (c *UrlController) RedirectShortenUrl(w http.ResponseWriter, r *http.Request) {
 	urlModel, err := models.NewUrlModel(c.dbConnection)
 	if err != nil{
-		w.WriteHeader(http.StatusInternalServerError)
+		http.Error(w, "error in make url model: " + err.Error(), http.StatusInternalServerError)
 	}else{
 		vars := mux.Vars(r)
 		mainUrl, err := urlModel.GetMainUrl(vars["unique_url_id"])
 		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
+			http.Error(w, "error in get url params: " + err.Error(), http.StatusInternalServerError)
 		}else{
 			http.Redirect(w, r, mainUrl, http.StatusSeeOther)
 		}
